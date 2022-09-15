c97b80fb1b9fad7f1da9ebdae30e9e5152cb6333
Tristan <jneal@breezeline.com>
2022-09-15 16:54:40 -0400

    timeline change. Next step is to begin work on modal

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,8 +4,10 @@ export const TimeLineData = [
     { year: 'March 2022', text: 'Began first solo developer cross-platform desktop application with Electron', },
     { year: 'April 2022', text: 'Took on new Client to build transactional email PWA to generate unique bingo cards', },
     { year: 'May 2022', text: 'Hired as remote full-stack developer for Drag Queen Seltzer & Hyperride Technologies', },
-    { year: 'June 2022', text: 'Started learning Blender and how to import 3D models into React projects', },
-    { year: 'July 2022', text: 'Integrated Chakra-UI into projects, and explored Elixer as first !OOP language', }
+    { year: 'June 2022', text: 'Began learning Blender and how to import 3D models into React projects', },
+    { year: 'July 2022', text: 'Integrated Chakra-UI into projects, and explored Elixer as first !OOP language', },
+    { year: 'August 2022', text: 'Applied to Breezeline Telecom for Software & Web Dev Positions', },
+    { year: 'September 2022', text: 'Breezeline: Successfully hired as Web Developer for in-house Frontend team', }
   ];
 
   export const skills = [
